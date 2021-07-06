3939feb7413fc0e298c145fc29e8e5eb3b0ded21
alessandro-fiorino <alessandro.fiorino@digitaldomus.it>
2021-07-06 14:00:14 +0200

    Black, isort and version bump

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "dd_external_id",
-
-    'summary': """
+    "name": "dd_external_id",
+    "summary": """
         External ID management made esier""",
-
-    'description': """
+    "description": """
         This modules adds a mixin allowing to expose the external XMLID of every record as a computed field
         It also allows to automatically generate XMLID for the records missing one (but only when forced to do so)
     """,
-
-    'author': "Digital Domus s.n.c.",
-    'website': "http://www.digitaldomus.it",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '13.0.1.0',
-
+    "author": "Digital Domus s.n.c.",
+    "website": "http://www.digitaldomus.it",
+    "category": "Uncategorized",
+    "version": "14.0.1.0",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        #'views/views.xml',
-        #'views/templates.xml',
-    ],
+    "depends": ["base"],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 }
--- models/models.py
+++ models/models.py
@@ -1,72 +1,97 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions
-import random, string
 import logging
+import random
+import string
+
+from odoo import api, exceptions, fields, models
+
 _logger = logging.getLogger(__name__)
 
 letters = string.ascii_lowercase
 
+
 def _random_string(length):
-    return ''.join(random.choice(letters) for i in range(length))
+    return "".join(random.choice(letters) for i in range(length))
+
 
 class ExternalIDMixin(models.AbstractModel):
-    _name = 'dd.externalid.mixin'
-    _description = 'Add external ID as virtual field'
-    _externalid_module = '__import__'
+    _name = "dd.externalid.mixin"
+    _description = "Add external ID as virtual field"
+    _externalid_module = "__import__"
     _externalid_addrandom = False
-    
-    
+
     def _externalid_gen(self):
         self.ensure_one()
         if self._externalid_addrandom:
-            return "%s_%d_%s" % (self._table,self.id,_random_string(8))
+            return "%s_%d_%s" % (self._table, self.id, _random_string(8))
         else:
-            return "%s_%d" % (self._table,self.id)
-    
+            return "%s_%d" % (self._table, self.id)
+
     def _calc_external_id(self):
-        exids = self.env['ir.model.data'].search([('model','=',self._name),
-                            ('res_id','in',self.ids)]).mapped(lambda k: {'id': k.res_id ,'s': k.module+'.'+k.name if k.id else ''})
-        dids={d['id']:d['s'] for d in exids}
-        if self.env.context.get('externalid_force_create',False):  
+        exids = (
+            self.env["ir.model.data"]
+            .search([("model", "=", self._name), ("res_id", "in", self.ids)])
+            .mapped(
+                lambda k: {"id": k.res_id, "s": k.module + "." + k.name if k.id else ""}
+            )
+        )
+        dids = {d["id"]: d["s"] for d in exids}
+        if self.env.context.get("externalid_force_create", False):
             _logger.info("externalid create")
-            m=self.env.context.get('externalid_module',self._externalid_module)
-            Imd=self.env['ir.model.data']
+            m = self.env.context.get("externalid_module", self._externalid_module)
+            Imd = self.env["ir.model.data"]
             for r in self:
-                if r.id!=0:
-                    v=dids.get(r.id,False)
+                if r.id != 0:
+                    v = dids.get(r.id, False)
                     if v:
                         r.external_id = v
                     else:
                         newid = r._externalid_gen()
-                        Imd.create({'name':newid, 'model': self._name, 'res_id': r.id, 'module': m, 'noupdate': False, })
-                        r.external_id = m+'.'+newid
+                        Imd.create(
+                            {
+                                "name": newid,
+                                "model": self._name,
+                                "res_id": r.id,
+                                "module": m,
+                                "noupdate": False,
+                            }
+                        )
+                        r.external_id = m + "." + newid
                 else:
                     r.externa_id = False
         else:
             for r in self:
-                r.external_id = dids.get(r.id,False)
-                
-    external_id = fields.Char(string="External ID (comp)", compute_sudo=True, compute='_calc_external_id')
-    
-    def set_external_id(self,xmlid):
+                r.external_id = dids.get(r.id, False)
+
+    external_id = fields.Char(
+        string="External ID (comp)", compute_sudo=True, compute="_calc_external_id"
+    )
+
+    def set_external_id(self, xmlid):
         self.ensure_one()
-        Imd=self.env['ir.model.data'].sudo()
-        r=Imd.search([('model','=',self._name),('res_id','=',self.id)])
-        if len(r)>0:
-            _logger.info("unlinking old ids %s" % (','.join(r.mapped('name'))))
+        Imd = self.env["ir.model.data"].sudo()
+        r = Imd.search([("model", "=", self._name), ("res_id", "=", self.id)])
+        if len(r) > 0:
+            _logger.info("unlinking old ids %s" % (",".join(r.mapped("name"))))
             r.unlink()
-        if xmlid.find('.')>=0:
-            m,newid=xmlid.split('.',1)
+        if xmlid.find(".") >= 0:
+            m, newid = xmlid.split(".", 1)
         else:
-            m,newid=self._externalid_module,xmlid
-        _logger.info("Setting xmlid %s" % (newid))        
-        Imd.create({'name':newid, 'model': self._name, 'res_id': self.id, 'module': m, 'noupdate': False, })
-        self.external_id = m+'.'+newid
-    
+            m, newid = self._externalid_module, xmlid
+        _logger.info("Setting xmlid %s" % (newid))
+        Imd.create(
+            {
+                "name": newid,
+                "model": self._name,
+                "res_id": self.id,
+                "module": m,
+                "noupdate": False,
+            }
+        )
+        self.external_id = m + "." + newid
+
 
 class ExternalIDPartner(models.Model):
-    _name = 'res.partner'
-    _inherit = ['res.partner','dd.externalid.mixin']
-    
-    
+    _name = "res.partner"
+    _inherit = ["res.partner", "dd.externalid.mixin"]
